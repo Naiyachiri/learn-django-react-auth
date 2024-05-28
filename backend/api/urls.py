8ca753b181d187e663008bc2452a0ad792888c76
James Bui <jamesvo.bui@core4ce.com>
2024-05-28 16:55:41 -0400

    Adds simple jwt to backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from . import views
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path("logout/", views.logout_view, name="api-logout"),
     path("session/", views.SessionView.as_view(), name="api-session"),
     path("whoami/", views.WhoAmIView.as_view(), name="api-whoami"),
+    path("token", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
 ]
