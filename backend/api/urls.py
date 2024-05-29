85f107ee4286a9455a666cf73ab07b550e5e77ed
James Bui <jamesvo.bui@core4ce.com>
2024-05-29 09:34:51 -0400

    Updates backend for JWT support
    - adds additional endpoints and configurations
    for the JWT usage

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import views
 
@@ -9,6 +12,6 @@ urlpatterns = [
     path("logout/", views.logout_view, name="api-logout"),
     path("session/", views.SessionView.as_view(), name="api-session"),
     path("whoami/", views.WhoAmIView.as_view(), name="api-whoami"),
-    path("token", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
