a0a22b6df3dad8cf52b6b07d4245e8623233af10
James Bui <jamesvo.bui@core4ce.com>
2024-05-29 15:57:24 -0400

    Dockerizes the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { logout, login, whoami, getSession } from "./api/fetch"
+import { jwtDecode } from "jwt-decode";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,9 +10,74 @@ class App extends React.Component {
       username: "",
       password: "",
       error: "",
+      access: "",
+      refresh: "",
       isAuthenticated: false,
+      daccess: "",
+      drefresh: "",
     };
   }
+
+  componentDidMount = () => {
+    // ensures the frontend has a csrf token so it can make api requests itself
+    if (!this.state.csrf && !this.state.isAuthenticated) {
+      this.getCSRF();
+    } else {
+      this.getSession();
+    }
+  };
+
+  getCSRF = () => {
+    fetch("/api/csrf/", {
+      credentials: "same-origin",
+    })
+      .then((res) => {
+        let csrfToken = res.headers.get("X-CSRFToken");
+        this.setState({ csrf: csrfToken });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  getSession = () => {
+    fetch("/api/session/", {
+      credentials: "same-origin",
+    })
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        if (data?.isAuthenticated) {
+          this.setState({ isAuthenticated: true });
+        } else {
+          this.setState({ isAuthenticated: false });
+          this.getCSRF();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  whoami = () => {
+    fetch("/api/whoami/", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.state.access}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log("You are logged in as: " + data.username);
+        this.setState({ whoami: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   handlePasswordChange = (event) => {
     this.setState({ password: event.target.value });
@@ -22,8 +87,62 @@ class App extends React.Component {
     this.setState({ username: event.target.value });
   };
 
-  componentDidMount = () => {
-    getSession();
+  // helper function
+  isResponseOk(response) {
+    if (response.status >= 200 && response.status <= 299) {
+      return response.json();
+    } else {
+      throw Error(response.statusText);
+    }
+  }
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    fetch("/api/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": this.state.csrf,
+      },
+      credentials: "same-origin",
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      }),
+    })
+      .then(this.isResponseOk)
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          isAuthenticated: true,
+          username: "",
+          password: "",
+          error: "",
+        });
+      })
+      .catch(() => {
+        this.setState({
+          error: "Unable to login. Wrong username or password.",
+        });
+      });
+  };
+
+  handleLogout = () => {
+    fetch("/api/logout", {
+      credentials: "same-origin",
+    })
+      .then(this.isResponseOk)
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          isAuthenticated: false,
+          csrf: "",
+        });
+        this.getCSRF();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   render() {
@@ -33,7 +152,7 @@ class App extends React.Component {
           <h1>React Cookie Auth</h1>
           <br />
           <h2>Login</h2>
-          <form onSubmit={login}>
+          <form onSubmit={this.handleLogin}>
             <div className='form-group'>
               <label htmlFor='username'>Username</label>
               <input
@@ -61,21 +180,25 @@ class App extends React.Component {
                 )}
               </div>
             </div>
-            <button type='submit' className='btn btn-primary'>
-              Login
-            </button>
           </form>
+          <button
+            className='btn btn-primary'
+            onClick={this.handleLogin}
+            disabled={!this.state.csrf}
+          >
+            Login
+          </button>
         </div>
       );
     }
     return (
       <div className='container mt-3'>
         <h1>React Cookie Auth</h1>
-        <p>You are logged in!</p>
-        <button className='btn btn-primary mr-2' onClick={whoami}>
+        <p>You are logged in! User</p>
+        <button className='btn btn-primary mr-2' onClick={this.whoami}>
           WhoAmI
         </button>
-        <button className='btn btn-danger' onClick={logout}>
+        <button className='btn btn-danger' onClick={this.handleLogout}>
           Log out
         </button>
       </div>
